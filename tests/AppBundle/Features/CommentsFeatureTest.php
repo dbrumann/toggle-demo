23ed38538059fed3f21f1b00961ac66bb248e77e
Denis Brumann <denis.brumann@sensiolabs.de>
2017-05-06 12:58:16 +0200

    Test functionality for Client B (comments disabled).

--- tests/AppBundle/Features/CommentsFeatureTest.php
+++ tests/AppBundle/Features/CommentsFeatureTest.php
@@ -9,30 +9,38 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class CommentsFeatureTest extends WebTestCase
 {
     /**
-     * @var Client
-     */
-    protected $client;
-
-    public function setUp()
-    {
-        $this->client = static::createClient();
-    }
-
-    /**
      * Client A wants a comments feature.
      *
      * When showing a single post, the user should see comments.
      */
     public function testCommentsAreAccessibleWhenEnabled()
     {
-        $crawler = $this->client->request('GET', $this->getShowBlogPostUri());
+        putenv('FEATURE_COMMENTS_ENABLED=1');
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $this->getShowBlogPostUri($client));
 
         $this->assertEquals(1, $crawler->filter('#main > h3')->count());
     }
 
-    protected function getShowBlogPostUri(): string
+    /**
+     * Client B does not want a comments feature.
+     *
+     * When showing a single post, there should be no comments displayed.
+     */
+    public function testCommentsAreNotVisibleWhenDisabled()
     {
-        $entityManager = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+        putenv('FEATURE_COMMENTS_ENABLED=0');
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $this->getShowBlogPostUri($client));
+
+        $this->assertEquals(0, $crawler->filter('#main > h3')->count());
+    }
+
+    protected function getShowBlogPostUri(Client $client): string
+    {
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
         $blogPostRepository = $entityManager->getRepository(Post::class);
         /** @var Post $blogPost */
         $blogPost = $blogPostRepository->findOneBy([], ['publishedAt' => 'ASC']);

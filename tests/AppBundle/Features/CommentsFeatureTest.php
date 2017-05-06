14c673706e1f15b05d9261d5b0340a082d435b98
Denis Brumann <denis.brumann@sensiolabs.de>
2017-05-06 13:17:43 +0200

    Implement and test functionality for Client C.

--- tests/AppBundle/Features/CommentsFeatureTest.php
+++ tests/AppBundle/Features/CommentsFeatureTest.php
@@ -16,6 +16,7 @@ class CommentsFeatureTest extends WebTestCase
     public function testCommentsAreAccessibleWhenEnabled()
     {
         putenv('FEATURE_COMMENTS_ENABLED=1');
+        putenv('FEATURE_COMMENTS_RESTRICTED=0');
         $client = static::createClient();
 
         $crawler = $client->request('GET', $this->getShowBlogPostUri($client));
@@ -31,11 +32,69 @@ class CommentsFeatureTest extends WebTestCase
     public function testCommentsAreNotVisibleWhenDisabled()
     {
         putenv('FEATURE_COMMENTS_ENABLED=0');
+        putenv('FEATURE_COMMENTS_RESTRICTED=0');
         $client = static::createClient();
 
         $crawler = $client->request('GET', $this->getShowBlogPostUri($client));
 
         $this->assertEquals(0, $crawler->filter('#main > h3')->count());
+    }
+
+    /**
+     * Client C wants comments enabled for a specific user group.
+     *
+     * When showing a single post, the user should not see comments
+     * unless they are logged in and have the right permissions.
+     */
+    public function testCommentsAreNotVisibleWhenFeatureIsRestrictedAndUserIsNotLoggedIn()
+    {
+        putenv('FEATURE_COMMENTS_ENABLED=1');
+        putenv('FEATURE_COMMENTS_RESTRICTED=1');
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $this->getShowBlogPostUri($client));
+
+        $this->assertEquals(0, $crawler->filter('#main > h3')->count());
+    }
+
+    /**
+     * Client C wants comments enabled for a specific user group.
+     *
+     * When showing a single post, the user should not see comments
+     * unless they are logged in and have the right permissions.
+     */
+    public function testCommentsAreNotVisibleWhenFeatureIsRestrictedAndUserIsNotAdmin()
+    {
+        putenv('FEATURE_COMMENTS_ENABLED=1');
+        putenv('FEATURE_COMMENTS_RESTRICTED=1');
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'john_user',
+            'PHP_AUTH_PW' => 'kitten',
+        ]);
+
+        $crawler = $client->request('GET', $this->getShowBlogPostUri($client));
+
+        $this->assertEquals(0, $crawler->filter('#main > h3')->count());
+    }
+
+    /**
+     * Client C wants comments enabled for a specific user group.
+     *
+     * When showing a single post, the user should not see comments
+     * unless they are logged in and have the right permissions.
+     */
+    public function testCommentsAreVisibleWhenFeatureIsRestrictedAndUserIsAdmin()
+    {
+        putenv('FEATURE_COMMENTS_ENABLED=1');
+        putenv('FEATURE_COMMENTS_RESTRICTED=1');
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'jane_admin',
+            'PHP_AUTH_PW' => 'kitten',
+        ]);
+
+        $crawler = $client->request('GET', $this->getShowBlogPostUri($client));
+
+        $this->assertEquals(1, $crawler->filter('#main > h3')->count());
     }
 
     protected function getShowBlogPostUri(Client $client): string

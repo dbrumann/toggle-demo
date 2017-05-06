cd6d05bba8e14e3983c15c2bb03ef38ac66c60bc
Denis Brumann <denis.brumann@sensiolabs.de>
2017-05-06 14:53:24 +0200

    Add restrictions to feature toggle.

--- src/AppBundle/FeatureToggle/Toggles/CommentsFeature.php
+++ src/AppBundle/FeatureToggle/Toggles/CommentsFeature.php
@@ -3,18 +3,23 @@
 namespace AppBundle\FeatureToggle\Toggles;
 
 use AppBundle\FeatureToggle\ToggleInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class CommentsFeature implements ToggleInterface
 {
+    private $authChecker;
     private $isEnabled;
+    private $isRestricted;
 
-    public function __construct(bool $isEnabled)
+    public function __construct(AuthorizationCheckerInterface $authChecker, bool $isEnabled, bool $isRestricted)
     {
+        $this->authChecker = $authChecker;
         $this->isEnabled = $isEnabled;
+        $this->isRestricted = $isRestricted;
     }
 
     public function isActive(): bool
     {
-        return $this->isEnabled;
+        return $this->isEnabled && (!$this->isRestricted || $this->authChecker->isGranted('ROLE_ADMIN'));
     }
 }
--- tests/AppBundle/FeatureToggles/Toggles/CommentsFeatureTest.php
+++ tests/AppBundle/FeatureToggles/Toggles/CommentsFeatureTest.php
@@ -3,20 +3,51 @@
 namespace Tests\AppBundle\FeatureToggle\Toggles;
 
 use AppBundle\FeatureToggle\Toggles\CommentsFeature;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class CommentsFeatureTest extends \PHPUnit_Framework_TestCase
 {
     public function testFeatureIsDisabled()
     {
-        $toggle = new CommentsFeature(false);
+        $authChecker = $this->getMock(AuthorizationCheckerInterface::class);
+        $toggle = new CommentsFeature($authChecker, false, false);
 
         $this->assertFalse($toggle->isActive());
     }
 
     public function testFeatureIsEnabled()
     {
-        $toggle = new CommentsFeature(true);
+        $authChecker = $this->getMock(AuthorizationCheckerInterface::class);
+        $toggle = new CommentsFeature($authChecker, true, false);
 
         $this->assertTrue($toggle->isActive());
+    }
+
+    public function testFeatureIsEnabledAndRoleAdminIsGranted()
+    {
+        $authChecker = $this->getMock(AuthorizationCheckerInterface::class);
+        $toggle = new CommentsFeature($authChecker, true, true);
+
+        $authChecker
+            ->expects($this->once())
+            ->method('isGranted')
+            ->with('ROLE_ADMIN')
+            ->willReturn(true);
+
+        $this->assertTrue($toggle->isActive());
+    }
+
+    public function testFeatureIsEnabledAndRoleAdminIsNotGranted()
+    {
+        $authChecker = $this->getMock(AuthorizationCheckerInterface::class);
+        $toggle = new CommentsFeature($authChecker, true, true);
+
+        $authChecker
+            ->expects($this->once())
+            ->method('isGranted')
+            ->with('ROLE_ADMIN')
+            ->willReturn(false);
+
+        $this->assertFalse($toggle->isActive());
     }
 }
